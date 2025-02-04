49e82eaf826a0096aac1cd8217ffac9ed07cf0d3
hanum <dlgksdma15@naver.com>
2025-02-04 20:35:57 +0900

    20250204 commit

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -1,7 +1,6 @@
 package hello.hello_spring.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,6 +41,7 @@ public class HelloController {
         public String getName() {
             return name;
         }
+
         public void setName(String name) {
             this.name = name;
         }
