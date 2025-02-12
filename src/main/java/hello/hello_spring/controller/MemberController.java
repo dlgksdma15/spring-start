7826c7b6286aeccc25dcdd8ceab1604874ab2022
hanum <dlgksdma15@naver.com>
2025-02-12 22:16:06 +0900

    20250212

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -21,12 +21,12 @@ public class MemberController {
     }
 
     @GetMapping("/members/new")
-    public String createForm(){
+    public String createForm() {
         return "members/createMemberForm";
     }
 
     @PostMapping("/members/new")
-    public String create(MemberForm form){
+    public String create(MemberForm form) {
         Member member = new Member(); // x001
         member.setName(form.getName()); // x001.setName(spring);
 
@@ -36,9 +36,9 @@ public class MemberController {
     }
 
     @GetMapping("/members")
-    public String list(Model model){
+    public String list(Model model) {
         List<Member> members = memberService.findMembers();
-        model.addAttribute("members",members);
-        return "members/memberList";
+        model.addAttribute("members", members);
+        return "members/memberList"; //
     }
 }
